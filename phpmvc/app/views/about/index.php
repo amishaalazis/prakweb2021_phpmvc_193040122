e934e73f6fb8a7ae7962929cde4d58c1f78c10f4
amishaalazis <60495662+amishaalazis@users.noreply.github.com>
2021-10-01 15:45:35 +0700

    menyelesaikan video aplikasi MVC pada PHP #6

--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,4 +1,7 @@
 
-    <h1>About Me</h1>
+  <div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/1.JPG" alt="amisha" width="200" class="rounded-circle" class= "shadow">
     <p>Hallo, nama saya <?= $data['nama'];?>, umur saya <?= $data['umur'];?> tahun. 
     Saya adalah seorang <?= $data['pekerjaan'];?></p>
+    </div>
